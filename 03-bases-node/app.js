bc4f4d49066262851da46abd5b15eb7c6d51dcbc
Irving <arthurlemus@hotmail.com>
2018-11-08 18:10:46 -0600

    Yargs
    uso de nueva libreria

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,3 +1,14 @@
+//Requireds
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        }
+    })
+    .argv;
+
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
 
@@ -6,13 +17,19 @@ const { crearArchivo } = require('./multiplicar/multiplicar');
 //============================
 //Obtener datos del process
 //============================
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+//let argv = process.argv;
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
 
+
+//============================
+//Obtener datos del process con Yargs
+//============================
+console.log(argv);
 
 //let base = '45';
 
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo Creado: ${archivo}`))
-    .catch(err => console.log(err));
+
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo Creado: ${archivo}`))
+//     .catch(err => console.log(err));
